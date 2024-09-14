edd09d3c50b6770f294f7c8aee0100667838e1c4
chengyunfei <chengyunfei@joyy.com>
2024-09-14 16:51:20 +0800

    删除body width限制

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
-
-  <!-- <img src="./assets/images/sensen.jpg" mode="scaleToFill"
-        style="width: 200px; height: 200px;background-color: red" /> -->
-  <div style="width: 100%; height: 100%; text-align: center">
-    <el-carousel trigger="click" type="card" height="600px">
+  <div style="width: 100%;  text-align: center">
+    <el-carousel trigger="click" height="400px">
       <el-carousel-item v-for="item in imageUrl" :key="item">
         <img :src="getAssetsFile(item.url)" alt="" style="height: 100%;" />
       </el-carousel-item>
@@ -19,7 +16,7 @@
     { url: "sensen.jpg" },
     { url: "kaixin.jpg" },
     { url: "keai.jpg" },
-    { url: "baitian.jpg" },]
+  ]
 
 </script>
 <style scoped
