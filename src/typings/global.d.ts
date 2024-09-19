ed576d390e582b199c7238dffa91635e4833e7f1
chengyunfei <chengyunfei@joyy.com>
2024-09-19 13:51:46 +0800

    删除敏感代码

--- src/typings/global.d.ts
+++ src/typings/global.d.ts
@@ -10,8 +10,6 @@ declare interface ViteEnv {
   VITE_PROXY_URL: string;
   VITE_BUILD_GZIP: boolean;
   VITE_REPORT: boolean;
-  VITE_APP_E_BRIDGE_LOGIN: string; // 商桥登录页地址
-  // VITE_PROXY_URL_CMCC: string; // 移动云
   VITE_PROXY_URL_DCMALL_PUB: string;
   VITE_APP_BASE_PATH: string;
   VITE_APP_BASE_URL: string;
